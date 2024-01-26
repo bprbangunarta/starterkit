d6dee5ce2505bf5a9c913c512d77e7b3a5180049
Zulfadli Rizal <zulfadlirizal@gmail.com>
2024-01-27 02:05:18 +0700

    Konfigurasi

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="/" class="app-brand-link">
+        <a href="{{ route('dashboard') }}" class="app-brand-link">
             <span class="app-brand-logo demo">
                 <x-app-logo></x-app-logo>
             </span>
